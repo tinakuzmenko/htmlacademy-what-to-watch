442fa7b53e1cf4929d9f9ebbe5ee29acc4a41661
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-06-12 16:00:06 +0200

    Добавляет в проект PropTypes

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ const Movie = {
   DATE: `2014`,
 };
 
+const moviesTitles = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+
 const root = document.querySelector(`#root`);
 
 ReactDOM.render(
     <App
       movieTitle={Movie.TITLE}
       movieGenre={Movie.GENRE}
-      movieDate={Movie.DATE} />,
+      movieDate={Movie.DATE}
+      moviesTitles={moviesTitles} />,
     root
 );
