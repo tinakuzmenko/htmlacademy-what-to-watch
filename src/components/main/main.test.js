d77065e15a30c30d8e8e0c83f8aba90ca2308adf
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-06-15 15:08:06 +0200

    Создает снепшот-тесты

---
+++ src/components/main/main.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const MOVIES_TITLES = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+
+describe(`Main`, () => {
+  it(`Should render correctly with movies titles array`, () => {
+    const tree = renderer
+      .create(<Main
+        movieTitle={`The Grand Budapest Hotel`}
+        movieGenre={`Drama`}
+        movieDate={`2014`}
+        moviesTitles={MOVIES_TITLES} />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/app/app.jsx";
-
-const Movie = {
-  TITLE: `The Grand Budapest Hotel`,
-  GENRE: `Drama`,
-  DATE: `2014`,
-};
-
-const moviesTitles = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`, `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`, `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`];
+import App from './components/app/app.jsx';
+import {Movie, MOVIES_TITLES} from './mocks/mocks.js';
 
 const root = document.querySelector(`#root`);
 
@@ -17,6 +10,6 @@ ReactDOM.render(
       movieTitle={Movie.TITLE}
       movieGenre={Movie.GENRE}
       movieDate={Movie.DATE}
-      moviesTitles={moviesTitles} />,
+      moviesTitles={MOVIES_TITLES} />,
     root
 );
