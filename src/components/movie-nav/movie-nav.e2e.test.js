99bd764656b8be0d5f8dcd1b459b7c4b9b83b68e
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-07-01 21:14:36 +0200

    Добавляет е2е-тест для компонента навигации

---
+++ src/components/movie-nav/movie-nav.e2e.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import MovieNav from './movie-nav.jsx';
+import {NavTabs} from '../../utils/constants.js';
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+const tabs = Object.values(NavTabs);
+
+describe(`MovieNav e2e tests`, () => {
+  it(`Should pass the right argument of current tab be clicked`, () => {
+    const onTabClick = jest.fn((args) => args);
+
+    const movieNavComponent = shallow(
+        <MovieNav
+          navTabs={NavTabs}
+          currentActiveTab={NavTabs.DETAILS}
+          onTabClick={onTabClick}
+        />
+    );
+
+    const movieNavTabs = movieNavComponent.find(`.movie-nav__link`);
+
+    movieNavTabs.forEach((movieNavTab, index) => {
+      movieNavTab.simulate(`click`, {preventDefault: onTabClick});
+      expect(onTabClick).toHaveBeenCalledWith(tabs[index]);
+    });
+  });
+});
