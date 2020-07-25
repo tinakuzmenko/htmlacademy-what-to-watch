8eb678fb3894415b22a66189e63486ba3de676f7
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-07-25 12:05:15 +0200

    Вносит правки по замечаниям наставника

--- src/api.js
+++ src/api.js
@@ -12,16 +12,16 @@ export const createAPI = () => {
     return response;
   };
 
-  const onFail = (err) => {
-    const {response} = err;
+  const onFail = (error) => {
+    const {response} = error;
 
     if (response.status === Error.UNAUTHORIZED) {
       // onUnauthorized();
 
-      throw err;
+      throw error;
     }
 
-    throw err;
+    throw error;
   };
 
   api.interceptors.response.use(onSuccess, onFail);
