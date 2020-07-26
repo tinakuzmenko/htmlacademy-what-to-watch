f4f70bfb59e07fbb6279c1c092b28f4f72459a4f
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-07-26 12:53:46 +0200

    Добавляет тесты для редьюсеров

---
+++ src/store/user/user.test.js
@@ -0,0 +1,82 @@
+import {initialState, ActionType, ActionCreator, reducer} from './user';
+import {AuthorizationStatus} from '../../helpers/constants';
+
+describe(`User Reducer`, () => {
+  it(`Reducer without additional parameters should return initial state`, () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it(`Reducer should change authorizationStatus by a given value`, () => {
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+
+  it(`Reducer should show Auth Error`, () => {
+    expect(reducer({
+      authorizationError: false,
+    }, {
+      type: ActionType.SHOW_AUTHORIZATION_ERROR,
+      payload: true,
+    })).toEqual({
+      authorizationError: true,
+    });
+  });
+
+  it(`Reducer should clear Auth Error`, () => {
+    expect(reducer({
+      authorizationError: true,
+    }, {
+      type: ActionType.CLEAR_AUTHORIZATION_ERROR,
+      payload: false,
+    })).toEqual({
+      authorizationError: false,
+    });
+  });
+});
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator for require authorization returns correct action`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+});
